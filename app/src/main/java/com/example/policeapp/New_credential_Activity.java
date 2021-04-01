c8bcb328ec287a7f68d3b6319a0403559f4ab7b1
Tanvi <tanvighanpathi@gmail.com>
2021-04-01 13:16:07 +0530

    Next Page Forgot password UI

---
+++ app/src/main/java/com/example/policeapp/New_credential_Activity.java
@@ -0,0 +1,14 @@
+package com.example.policeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class New_credential_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_credential_);
+    }
+}
